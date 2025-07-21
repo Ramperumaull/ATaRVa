f65c45323f4e2697deb34d131ea2d34707868c4f
Dhaksnamoorthy <abisheakkumar141@gmail.com>
2025-07-21 20:52:08 +0530

    solved motif error

--- ATARVA/vcf_writer.py
+++ ATARVA/vcf_writer.py
@@ -73,10 +73,11 @@ def vcf_homozygous_writer(ref, contig, locus_key, global_loci_info, homozygous_a
 
     if decomp:
         motif_size = int(float(global_loci_info[locus_key][4]))
+        motif = global_loci_info[locus_key][3]
         # FORMAT = 'GT:AL:SD:PC:DP:SN:SQ:DS'
         FORMAT = 'GT:AL:SD:DP:SN:SQ:DS'
         if alt_state & (motif_size<=10):
-            deseq = motif_decomposition(ALT, motif_size)
+            deseq = motif_decomposition(ALT, motif_size, motif)
         else:
             deseq = '.'
         # SAMPLE = str(GT) + ':' + str(homozygous_allele) + ',' + str(homozygous_allele) + ':' + str(reads_len) + ':.:' + str(DP) + ':.:.' + ':' + deseq
@@ -177,6 +178,7 @@ def vcf_heterozygous_writer(contig, genotypes, locus_start, global_loci_variatio
 
     if decomp:
         motif_size = int(float(global_loci_info[locus_key][4]))
+        motif = global_loci_info[locus_key][3]
         # FORMAT = 'GT:AL:SD:PC:DP:SN:SQ:DS'
         FORMAT = 'GT:AL:SD:DP:SN:SQ:DS'
         if motif_size>10:
@@ -185,7 +187,7 @@ def vcf_heterozygous_writer(contig, genotypes, locus_start, global_loci_variatio
             ds = []
             for iseq in alt_seqs:
                 if iseq:
-                    ds.append(motif_decomposition(iseq, motif_size))
+                    ds.append(motif_decomposition(iseq, motif_size, motif))
                 else:
                     ds.append('.')
             deseq = ','.join(ds)
